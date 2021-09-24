cf225f9c81c24d5af8124312b4c524d8eb25dd21
brookecalhoun <86681030+brookecalhoun@users.noreply.github.com>
2021-09-24 09:13:39 -0700

    styling

--- src/components/Resources/AddictionResources.jsx
+++ src/components/Resources/AddictionResources.jsx
@@ -13,18 +13,16 @@ class AddictionResources extends React.Component {
   render() {
     return (
       <div className="addiction-page">
+        <br />
         <h1 className="ad-title">Addiction Resources </h1>
+        <br />
         <Tabs
           className="tab-color"
           id="controlled-tab-example"
           activeKey={this.state.key}
           onSelect={(key) => this.setState({ key })}
         >
-          <Tab
-            className="pop-tab"
-            eventKey="population"
-            title="search by population"
-          >
+          <Tab className="pop-tab" eventKey="population" title="by population">
             <div className="ad-populations">
               {" "}
               <h3 className="ad-lgbt">
@@ -79,31 +77,40 @@ class AddictionResources extends React.Component {
               </h3>{" "}
             </div>
           </Tab>
-          <Tab
-            className="ad-tab"
-            eventKey="addiction"
-            title="search by addiction"
-          >
+          <Tab className="ad-tab" eventKey="addiction" title="by addiction">
             <div className="by-addiction">
               <div className="alcohol">
                 <h2 className="alcohol-title">alcohol</h2>
                 <h3 className="alcohol-1">
-                  <a href="https:www.aa.org/"
-                  style={{ textDecoration: "none " }}>alcoholics anonymous</a>
+                  <a
+                    className="alink-1"
+                    href="https:www.aa.org/"
+                    style={{ textDecoration: "none " }}
+                  >
+                    alcoholics anonymous
+                  </a>
                 </h3>
               </div>
               <div className="cocaine">
                 <h2 className="cocaine-title">cocaine</h2>
                 <h3 className="cocaine-1">
-                  <a href="https://ca.org/"
-                  style={{ textDecoration: "none " }}>cocaine anonymous</a>
+                  <a
+                    href="https://ca.org/"
+                    style={{ textDecoration: "none " }}
+                    className="alink-2"
+                  >
+                    cocaine anonymous
+                  </a>
                 </h3>
               </div>
               <div className="marijuana">
                 <h2 className="marijuana-title">marijuana</h2>
                 <h3 className="marijuana-1">
-                  <a href="https://marijuana-anonymous.org/"
-                  style={{ textDecoration: "none " }}>
+                  <a
+                    className="alink-3"
+                    href="https://marijuana-anonymous.org/"
+                    style={{ textDecoration: "none " }}
+                  >
                     marijuana anonymous
                   </a>
                 </h3>
@@ -111,8 +118,11 @@ class AddictionResources extends React.Component {
               <div className="methamphetamine">
                 <h2 className="methamphetamine-title">methamphetamine</h2>
                 <h3 className="methamphetamine-1">
-                  <a href="https://www.crystalmeth.org/"
-                  style={{ textDecoration: "none " }}>
+                  <a
+                    className="alink-4"
+                    href="https://www.crystalmeth.org/"
+                    style={{ textDecoration: "none " }}
+                  >
                     methamphetamine anonymous
                   </a>
                 </h3>
@@ -120,57 +130,99 @@ class AddictionResources extends React.Component {
               <div className="Opiods">
                 <h2 className="opiod-title">opiods</h2>
                 <h3 className="opiod-1">
-                  <a href="https://www.na.org/"
-                  style={{ textDecoration: "none " }}>narcotics anonymous</a>
+                  <a
+                    className="alink-5"
+                    href="https://www.na.org/"
+                    style={{ textDecoration: "none " }}
+                  >
+                    narcotics anonymous
+                  </a>
                 </h3>
                 <h3 className="opiod-3">
-                  <a href="https://www.neveraloneclub.org/"
-                  style={{ textDecoration: "none " }}>never alone club</a>
+                  <a
+                    className="alink-6"
+                    href="https://www.neveraloneclub.org/"
+                    style={{ textDecoration: "none " }}
+                  >
+                    never alone club
+                  </a>
                 </h3>
               </div>
             </div>
           </Tab>
-          <Tab className='support-tab'eventKey="support" title="other sources of support">
+          <Tab
+            className="support-tab"
+            eventKey="support"
+            title="other sources of support"
+          >
             <div className="ad-support">
-              {" "}
-              <h3 className="acoa">
+              <br />
+              <h3>SAMHSA national helpline:</h3>
+              <p>1-800-662-HELP(4357)</p>{" "}
+              <h3 className="sup-1">
                 {" "}
-                <a href="https://adultchildren.org/"
-                style={{ textDecoration: "none " }}>
+                <a
+                  href="https://adultchildren.org/"
+                  style={{ textDecoration: "none " }}
+                  className="support-link-1"
+                >
                   adult children of alcoholics{" "}
                 </a>{" "}
               </h3>{" "}
-              <h3 className="alanon">
-                <a href="https://al-anon.org/"
-                style={{ textDecoration: "none " }}>al-anon</a>{" "}
+              <br />
+              <h3 className="sup-2">
+                <a
+                  href="https://al-anon.org/"
+                  style={{ textDecoration: "none " }}
+                  className="support-link-2"
+                >
+                  al-anon
+                </a>{" "}
               </h3>{" "}
-              <h3 className="naranon">
-                <a href="https://www.nar-anon.org/"
-                style={{ textDecoration: "none " }}>nar-anon</a>{" "}
+              <br />
+              <h3 className="sup-3">
+                <a
+                  href="https://www.nar-anon.org/"
+                  style={{ textDecoration: "none " }}
+                  className="support-link-3"
+                >
+                  nar-anon
+                </a>{" "}
               </h3>{" "}
-              <h3 className="dfk">
+              <br />
+              <h3 className="sup-4">
                 {" "}
-                <a href="https://drugfree.org/"
-                style={{ textDecoration: "none " }}>
+                <a
+                  href="https://drugfree.org/"
+                  style={{ textDecoration: "none " }}
+                  className="support-link-4"
+                >
                   partnership for drug-free kids{" "}
                 </a>{" "}
               </h3>{" "}
-              <h3 className="books">
+              <br />
+              <h3 className="sup-5">
                 {" "}
-                <a href="https://www.smartrecovery.org/suggested-reading-list/"
-                style={{ textDecoration: "none " }}>
+                <a
+                  href="https://www.smartrecovery.org/suggested-reading-list/"
+                  style={{ textDecoration: "none " }}
+                  className="support-link-5"
+                >
                   suggested books{" "}
                 </a>{" "}
               </h3>{" "}
-              <h3 className="podcasts">
+              <br />
+              <h3 className="sup-6">
                 {" "}
-                <a href="https://www.therecoveryvillage.com/recovery/related/addiction-recovery-podcasts/"
-                style={{ textDecoration: "none " }}>
+                <a
+                  href="https://www.therecoveryvillage.com/recovery/related/addiction-recovery-podcasts/"
+                  style={{ textDecoration: "none " }}
+                  className="support-link-6"
+                >
                   suggested podcasts{" "}
                 </a>{" "}
               </h3>{" "}
-              <h3>SAMHSA national helpline:</h3>
-              <p>1-800-662-HELP(4357)</p>
+              <br />
             </div>
           </Tab>
         </Tabs>
